9b05b404b395b1f618afd34807b4feddded6e2fd
sabharishsrinivas <69449463+sabharishsrinivas@users.noreply.github.com>
2020-08-10 12:20:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBKyv34hky3VgxmXTaxywOT-48xhM11gJo",
+  authDomain: "phishing-3472b.firebaseapp.com",
+  databaseURL: "https://phishing-3472b.firebaseio.com",
+  projectId: "phishing-3472b",
+  storageBucket: "phishing-3472b.appspot.com",
+  messagingSenderId: "356197607322",
+  appId: "1:356197607322:web:e8e61f0d75a1b5feacfc23",
+  measurementId: "G-30EXDX05BG"
 };
 
      
